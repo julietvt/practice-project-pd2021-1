9ff7439cb3b4a40b42f9e22c8625b4bb90014700
julietvt <freshitpt@gmail.com>
2021-12-05 13:37:54 +0200

    FaqArticlesList was created for registration page

---
+++ client/src/components/FaqArticlesList/FaqArticlesList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './FaqArticlesList.module.sass';
+import faqs from './faqs.json';
+import CONSTANTS from './../../constants';
+
+function FaqArticlesList() {
+  return (
+    <div className={styles.articlesMainContainer}>
+      {faqs.map((col, i) => (
+        <div key={i} className={styles.ColumnContainer}>
+          {col.map((articles, j) => (
+            <div key={j}>
+              <div className={styles.headerArticle}>{articles.header}</div>
+              <div className={styles.article}>{articles.body}</div>
+            </div>
+          ))}
+          {i == 1 && (
+            <>
+              <div className={styles.headerArticle}>
+                I have other questions! How can I get in touch with Squadhelp?
+              </div>
+              <div className={styles.article}>
+                Check out our <span className={styles.orangeSpan}>FAQs</span> or
+                send us a<span className={styles.orangeSpan}>message</span>. For
+                assistance with launching a contest, you can also call us at
+                {CONSTANTS.CONTACTS.TEL} or schedule a
+                <span className={styles.orangeSpan}>Branding Consultation</span>
+              </div>
+            </>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default FaqArticlesList;
